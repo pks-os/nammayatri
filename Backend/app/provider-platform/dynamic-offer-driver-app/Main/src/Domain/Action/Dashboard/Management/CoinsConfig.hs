47b908d63ef59b1234cda61d54c29b7ca031df00
Shamil Nizamov <na-80@yandex.ru>
2025-01-27 17:12:35 +0500

    Added getVehicleCategoryFromMaybe function

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/Dashboard/Management/CoinsConfig.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/Dashboard/Management/CoinsConfig.hs
@@ -10,11 +10,13 @@ import qualified API.Types.ProviderPlatform.Management.CoinsConfig as Common
 import Data.OpenApi (ToSchema)
 import qualified Domain.Types.Coins.CoinsConfig as DTCC
 import qualified Domain.Types.Merchant
+import Domain.Types.VehicleCategory (VehicleCategory)
 import qualified Environment
 import EulerHS.Prelude hiding (id)
 import Kernel.Types.APISuccess (APISuccess (Success))
 import qualified Kernel.Types.Beckn.Context
 import qualified Kernel.Types.Id as ID
+import qualified Kernel.Utils.Common as UC
 import Servant
 import qualified Storage.CachedQueries.CoinsConfig as CQConfig
 import qualified Storage.Queries.Coins.CoinsConfig as QConfig
@@ -36,10 +38,7 @@ putCoinsConfigUpdate _merchantShortId _opCity Common.UpdateReq {..} = do
                 DTCC.expirationAt = expirationAt,
                 DTCC.coins = coins
               }
-          vehicleCategory =
-            case coinsConfig.vehicleCategory of
-              Just val -> val
-              Nothing -> error "vehicleCategory is Nothing" coinsConfig.vehicleCategory
+          vehicleCategory = getVehicleCategoryFromMaybe coinsConfig.vehicleCategory
       _ <- QConfig.updateCoinEntries updatedCoinsConfig
       _ <- CQConfig.clearCache coinsConfig.eventName coinsConfig.eventFunction (ID.Id coinsConfig.merchantOptCityId) vehicleCategory
       pure Success
@@ -49,4 +48,36 @@ postCoinsConfigCreate ::
   Kernel.Types.Beckn.Context.City ->
   Common.CreateCoinsConfigReq ->
   Environment.Flow Kernel.Types.APISuccess.APISuccess
-postCoinsConfigCreate _merchantShortId _opCity req = do error "Logic yet to be decided" req
+postCoinsConfigCreate _merchantShortId _opCity req = do
+  uuid <- UC.generateGUIDText
+  newCoinsConfig <- case req of
+    Common.NewCoinsConfig (Common.NewCoinsConfigReq {..}) ->
+      pure $
+        DTCC.CoinsConfig
+          { id = ID.Id uuid,
+            ..
+          }
+    Common.DuplicateCoinsConfig (Common.DuplicateCoinsConfigsReq {..}) -> do
+      mbCoinsConfig <- QConfig.fetchConfigOnIdBasis $ ID.cast entriesId
+      case mbCoinsConfig of
+        Nothing -> error "No entries with such Id:" entriesId
+        Just coinsConfig ->
+          pure $
+            coinsConfig
+              { DTCC.id = ID.Id uuid,
+                DTCC.eventFunction = eventFunction
+              }
+  _ <- QConfig.updateCoinEntries newCoinsConfig
+  _ <-
+    CQConfig.clearCache
+      newCoinsConfig.eventName
+      newCoinsConfig.eventFunction
+      (ID.Id newCoinsConfig.merchantOptCityId)
+      (getVehicleCategoryFromMaybe newCoinsConfig.vehicleCategory)
+  pure Success
+
+getVehicleCategoryFromMaybe :: Maybe VehicleCategory -> VehicleCategory
+getVehicleCategoryFromMaybe mbVehicleCategory =
+  case mbVehicleCategory of
+    Just vehicleCategory -> vehicleCategory
+    Nothing -> error "vehicleCategory is Nothing" mbVehicleCategory
